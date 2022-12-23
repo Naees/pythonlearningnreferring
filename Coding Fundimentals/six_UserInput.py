1f522685f2e3169b5c2cb9c3483c0c39bb53242f
Naees <bubblesproximacenturion@gmail.com>
2022-12-24 00:39:35 +0800

    Collecting user inputs and displaying them

---
+++ Coding Fundimentals/six_UserInput.py
@@ -0,0 +1,15 @@
+# 6. User Inputs
+
+name = "Naees"
+age = int(input("How old are you: "))
+height = float(input("How tall are you?: "))
+
+#print(name.find("e"))
+print(name.capitalize())
+print(name.lower())
+print(name.upper())
+print(name.isdigit())
+
+print("Hello "+name)
+print("You are " +str(age)+ " years old")
+print("You are " +str(height)+ "cm tall")
