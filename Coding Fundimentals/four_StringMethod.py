c9084e9e8b2c49e2876b8eae86500786c0ca5eca
Naees <bubblesproximacenturion@gmail.com>
2022-12-24 00:18:33 +0800

    Playing around with string/text

---
+++ Coding Fundimentals/four_StringMethod.py
@@ -0,0 +1,13 @@
+name = "Naees"
+namechange = "Sean"
+
+print(len(name))
+print(name.find("e"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("a"))
+print(namechange.replace("Sean", "Naees"))
+print(name*3)
