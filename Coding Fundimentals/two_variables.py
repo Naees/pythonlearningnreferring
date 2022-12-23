49d00bc0cd8129a96394372eae035562105e0355
Naees <bubblesproximacenturion@gmail.com>
2022-12-24 00:09:30 +0800

    Variable fundimentals

---
+++ Coding Fundimentals/two_variables.py
@@ -0,0 +1,23 @@
+# multiple assignment = allows us to assign multiple variables at the same time in one line of code
+
+# name = "John Doe"
+# age = 22
+# postive = True
+
+# name, age, attractive = "Bro", 21, True
+
+# print(name)
+# print(age)
+# print(postive)
+
+Rochelle = 50
+Nick = 40
+Ellis = 27
+Coach = 19
+
+# Rochelle, Nick, Ellis, Coach = 999
+
+print(Rochelle)
+print(Nick)
+print(Ellis)
+print(Coach)
