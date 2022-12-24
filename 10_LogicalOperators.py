8edcec705c33a2a126b934e5e0a1a79f6f8d2888
Naees <bubblesproximacenturion@gmail.com>
2022-12-24 16:22:20 +0800

    More infomation about logical operators

--- 10_LogicalOperators.py
+++ 10_LogicalOperators.py
@@ -7,7 +7,15 @@ temp = int(input("What is the temperature outside?: "))
 if (temp>=0 and temp<= 30):
   print("The temperature is good today!")
   print("Go outside")
+  print()
 
 elif (temp < 0 or temp >30):
   print("The temperature is bad today!")
   print("Stay inside")
+  print()
+
+
+# not operator example
+if not(temp>=0 and temp<= 30):
+  print("The weather is bad today!")
+  print("Lets stay at home")
