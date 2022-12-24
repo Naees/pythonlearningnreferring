43d5f6c8a2756e3048f8775daa4a4111120fd85a
Naees <bubblesproximacenturion@gmail.com>
2022-12-24 11:34:13 +0800

    Addition of Math Functions

---
+++ Coding Fundimentals/7_MathFunctions.py
@@ -0,0 +1,17 @@
+#7. Math Functions
+import math
+
+pi = 3.14
+mathx = 1
+mathy = 2
+mathz = 3
+
+
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(pi))
+print(pow(pi,2))
+print(math.sqrt(pi))
+print(max(mathx,mathy,mathz))
+print(min(mathx,mathy,mathz))
