7f428433162b1ef4b9401482b4950a13ee1a047d
Naees <bubblesproximacenturion@gmail.com>
2022-12-24 16:13:19 +0800

    These are some examples of logical operators

---
+++ 10_LogicalOperators.py
@@ -0,0 +1,13 @@
+# 10. logical operators
+
+# logical operats (and,or,not) = used to check if two or more conditional statements
+
+temp = int(input("What is the temperature outside?: "))
+
+if (temp>=0 and temp<= 30):
+  print("The temperature is good today!")
+  print("Go outside")
+
+elif (temp < 0 or temp >30):
+  print("The temperature is bad today!")
+  print("Stay inside")
