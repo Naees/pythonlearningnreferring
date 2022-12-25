e2dcae61c9b4280c84828afb8e574651c7370e55
Naees <bubblesproximacenturion@gmail.com>
2022-12-26 02:31:45 +0800

    Simple implementation on some indexing operations

---
+++ 20_Indexing.py
@@ -0,0 +1,9 @@
+# 20. Indexing
+# index operator [] = gives access to a sequence's element (str, list, tuples)
+
+vidgame = 'tabs Totally Accurate Battle Simulator'
+
+if(vidgame[0].islower()):
+    vidgame = vidgame.capitalize()
+
+print(vidgame)
