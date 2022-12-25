2ec66f88e022d0933e7a3c07e529eb41479d0c4d
Naees <bubblesproximacenturion@gmail.com>
2022-12-25 15:21:00 +0800

    This shows how list could be implemented in 2D format

---
+++ 16_TwoDimensionalList.py
@@ -0,0 +1,9 @@
+# 2D list = a list of list
+
+places = ["Orchard Road", "Washington Plaza", "Don Quijote"]
+fruits = ["Apples", "Pears", "Mangos"]
+food = ["Chicken Rice", "Grilled Sticks", "Curry and Rice"]
+
+twoDList = [places, fruits, food]
+
+print(twoDList)
