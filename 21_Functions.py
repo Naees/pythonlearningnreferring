5fe373037ff649f6d4cdc660525519e7a84bc2ad
Naees <bubblesproximacenturion@gmail.com>
2022-12-26 14:07:18 +0800

    Functions and how to use them

---
+++ 21_Functions.py
@@ -0,0 +1,23 @@
+# function = a block of code which is executed only when it is called.
+
+def my_first_function():
+    print("This is my first function!")
+    print("Yay!")
+    print()
+my_first_function()
+
+# functions with user input
+def a_function_with_inputs(username):
+    print(f"Hi my name is {username}")
+    print()
+
+userinput= input("What is your name: ")
+a_function_with_inputs(userinput)
+
+#functions with multiple preset variables
+def func_with_multiple_variables(fname, lname, age):
+    print(f"Hello {fname} {lname}, you are {age} years old!")
+    print("Thank you for your time :) ")
+func_with_multiple_variables("Whip", "Nae Nae", 22)
+presetvar = "Naees"
+
