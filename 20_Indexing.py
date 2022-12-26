c09a4f5b165db8e16097fe75c574cee5f1e2e3cf
Naees <bubblesproximacenturion@gmail.com>
2022-12-26 14:11:50 +0800

    Simple implementation on indexing operations

--- 20_Indexing.py
+++ 20_Indexing.py
@@ -1,9 +1,17 @@
 # 20. Indexing
 # index operator [] = gives access to a sequence's element (str, list, tuples)
 
-vidgame = 'tabs Totally Accurate Battle Simulator'
+vidgame = 'tabs Totally Accurate Battle Simulator!'
 
 if(vidgame[0].islower()):
     vidgame = vidgame.capitalize()
-
 print(vidgame)
+print()
+
+first = vidgame[:3].upper()
+last = vidgame[4:].lower()
+lChar = vidgame[-1]
+
+print(first)
+print(last)
+print(lChar)
