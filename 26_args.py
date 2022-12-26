25011f4ed144397ea5cf5094295f8d6b3e585935
Naees <bubblesproximacenturion@gmail.com>
2022-12-26 18:40:09 +0800

    c:; cd 'c:\Users\bubbl\pythonlearningnreferring'; & 'C:\Users\bubbl\AppData\Local\Programs\Python\Python311\python.exe' 'c:\Users\bubbl\.vscode\extensions\ms-python.python-2022.20.1\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher' '64901' '--' 'c:\Users\bubbl\pythonlearningnreferring\26_args.py'

---
+++ 26_args.py
@@ -0,0 +1,12 @@
+# *args =   parameter that will pack all arguements into a tuple
+#           useful so that a function can accept a varying amout of arguements
+
+def sumofnumbers(*args): #*args can be different string 
+    sum = 0
+    addition = list(args)   # as you can't alter the information in the tuple "*args variable", there is a need to cast "*args" into a list for editing
+    addition[0] = 20
+    for i in addition:
+        sum += i
+    return sum
+
+print(sumofnumbers(1,1,1,1))
