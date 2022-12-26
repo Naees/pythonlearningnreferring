38f47fcb1a3a67eb74a88173b981403b7d40b0a3
Naees <bubblesproximacenturion@gmail.com>
2022-12-26 18:42:03 +0800

    *args for functions and also how to edit the *args tuple

--- 26_args.py
+++ 26_args.py
@@ -1,7 +1,7 @@
 # *args =   parameter that will pack all arguements into a tuple
 #           useful so that a function can accept a varying amout of arguements
 
-def sumofnumbers(*args): #*args can be different string 
+def sumofnumbers(*args): #*args can be different parameter
     sum = 0
     addition = list(args)   # as you can't alter the information in the tuple "*args variable", there is a need to cast "*args" into a list for editing
     addition[0] = 20
