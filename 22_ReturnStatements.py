5c8cabb6d1a7a408a969b827ee6d22c2d1555a5c
Naees <bubblesproximacenturion@gmail.com>
2022-12-26 17:02:03 +0800

    Implementation of return statement with the use of a function

---
+++ 22_ReturnStatements.py
@@ -0,0 +1,7 @@
+# return statement =    Function send Python values/objects back to the caller.
+#                       These values/objects are known as the functions' return value
+
+def division(x,y):
+    return x/y
+
+print(division(8,2))
