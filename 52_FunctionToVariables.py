be3c523f78b593c88c7215e510de025bcf1f874b
Naees <bubblesproximacenturion@gmail.com>
2023-02-13 01:08:04 +0800

    Changing functions to variables to access them instead of  memory space

---
+++ 52_FunctionToVariables.py
@@ -0,0 +1,10 @@
+def samplefunction():
+    print("This is a sample function..")
+    
+alternativeFunctionName = samplefunction
+alternativeFunctionName
+samplefunction
+
+intConversionFunction = int                 # This is an alternative way to call functions
+print(intConversionFunction("100") - 5)     # Here I changed the string of 100 to int of 100 and subtracted it by 5 to prove that to intConversionFunction works
+
