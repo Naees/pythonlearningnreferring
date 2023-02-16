762c28308aecb7060f776dd1d1f76995745c15a5
Naees <bubblesproximacenturion@gmail.com>
2023-02-17 00:42:22 +0800

    Understand how modules can be run as a standalone program and how module can be imported and used by other module

---
+++ 62_IfName=='__main__'.py
@@ -0,0 +1,18 @@
+# if __name__ == '__main__'
+
+# y tho?
+# 1. Module can be run as a standalone program
+#   or
+# 2. Module can be imported and used by other module
+
+# Python interpreter set "special variables", one of which is __name__
+# Python will assign the __name__ variable a value of '__main__' if it's
+# the initial module being run
+
+
+def someImportantFunction():
+    print("This is an important function!!")
+
+
+if __name__== '__main__':
+    pass 
