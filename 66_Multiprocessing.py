be6b44a41d03bcb50732cbffe15abf83709ee61e
Naees <bubblesproximacenturion@gmail.com>
2023-02-19 23:01:18 +0800

    running tasks in parallel on different cpu cores, bypasses GIL used for thread

---
+++ 66_Multiprocessing.py
@@ -0,0 +1,49 @@
+# Multiprocessing = running tasks in parallel on different cpu cores, bypasses GIL used for thread
+#                   multiprocessing =   better for cpu bound task (heavy cpu usage)
+#                   multithreading =    better for io bound task (waiting around)
+
+from multiprocessing import Process, cpu_count
+import time
+
+def counter(num):
+    count = 0
+    while count < num:
+        count += 1
+
+def main():
+
+    print(cpu_count())
+    
+    a = Process(target=counter, args=(1000000, ))
+    b = Process(target=counter, args=(1000000, ))
+    c = Process(target=counter, args=(1000000, ))
+    d = Process(target=counter, args=(1000000, ))
+    e = Process(target=counter, args=(1000000, ))
+    f = Process(target=counter, args=(1000000, ))
+    g = Process(target=counter, args=(1000000, ))
+    h = Process(target=counter, args=(1000000, ))
+    
+    
+    a.start()
+    b.start()
+    c.start()
+    d.start()
+    e.start()
+    f.start()
+    g.start()
+    h.start()
+
+
+    a.join()
+    b.join()
+    c.join()
+    d.join()
+    e.join()
+    f.join()
+    g.join()
+    h.join()
+
+    print(f"Finished in: {time.perf_counter()} seconds")
+
+if __name__ == '__main__':
+    main()
