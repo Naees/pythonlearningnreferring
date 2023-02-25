bc72261f1c1d2492deb55a4ad8863773778a47b9
Naees <bubblesproximacenturion@gmail.com>
2023-02-25 18:09:54 +0800

    With tkinter learn how to make and manage checkboxes

--- 071_Checkbox.py
+++ 071_Checkbox.py
@@ -1,19 +1,32 @@
 from tkinter import *
 
 def Display():
-    if (x.get()==1):
+    if (x.get()):
         print("You have agreed to the terms and conditions.")
     else:
         print("You have not agreed to the terms and conditions, please accept to continue.")
-
+        
 window = Tk()
-x = IntVar()
+x = BooleanVar()
+# Here are some of the other choices depending on what var you are accepting
+# IntVar
+# StringVar
+image = PhotoImage(file='small_icon.png')
 check_button = Checkbutton(window, 
                            text="Click here to accept the terms and conditions.\nMore info below..",
                            variable=x,
-                           onvalue=1,
-                           offvalue=0,
-                           command= Display)
+                           onvalue=True,
+                           offvalue=False,
+                           command=Display,
+                           font=('Comic San', 20),
+                           fg='blue',
+                           bg="black",
+                           activebackground='white',
+                           activeforeground='purple',
+                           padx=25,
+                           pady=10,
+                           image=image,
+                           compound=LEFT)
 
 check_button.pack()
 window.mainloop()
