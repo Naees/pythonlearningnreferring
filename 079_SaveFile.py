529368b70541d893ceace614d12bc32254af664f
Naees <bubblesproximacenturion@gmail.com>
2023-02-27 21:12:20 +0800

    With tkinter learn how to save file

--- 079_SaveFile.py
+++ 079_SaveFile.py
@@ -2,11 +2,15 @@ from tkinter import *
 from tkinter import filedialog
 
 def saveFile():
-    file = filedialog.asksaveasfile(defaultextension=".txt", 
+    file = filedialog.asksaveasfile(initialdir="/home/naees/Desktop",
+                                    defaultextension=".txt", 
                                     filetypes=[("Save as text file", ".txt"),
                                               ("Save as HTML file", ".html"),
                                               ("Save as any file", ".*")])
+    if file is None:
+        return
     filetext = str(text.get(1.0, END))
+    # filetext = input("Enter the text that you want to be saved: ")
     file.write(filetext)
     file.close()
 
