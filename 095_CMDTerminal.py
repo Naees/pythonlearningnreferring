5afa0c54fb3c17773993403e9091b949ead4ef33
Naees <bubblesproximacenturion@gmail.com>
2023-03-03 00:47:44 +0800

    Learn how to run py scripts with terminal/cmd

---
+++ 095_CMDTerminal.py
@@ -0,0 +1,12 @@
+# run .py file with cmd
+
+# save file as .py (Python file)
+# go to command prompt
+# navigate to directory w/ your file
+# invoke python interpreter + script: python 
+
+print("The Naees Terminal Test")
+
+username = input("What is your name: ")
+
+print(f"Your name is {username}")
